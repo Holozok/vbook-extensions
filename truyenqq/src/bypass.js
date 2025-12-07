9d696fc2935d235f97e5850bf846c97e82696bda
Cuong <171318302+Holozok@users.noreply.github.com>
2025-12-07 00:04:51 -0600

    update truyenqq

--- truyenqq/src/bypass.js
+++ truyenqq/src/bypass.js
@@ -2,7 +2,12 @@ function bypass(url, doc) {
     var cookie = doc.html().match(/document.cookie="(.*?)"/);
     if (cookie) {
         cookie = cookie[1];
-        doc = Http.get(url).headers({"Cookie": cookie}).html();
+        doc = Http.get(url)
+            .headers({
+                "Cookie": cookie,
+                "User-Agent": "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36"
+            })
+            .html();
     }
     return doc
 }
