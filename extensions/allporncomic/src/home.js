980a28d7f45200fa74aa4073f5188c65fda3292c
Cuong <171318302+Holozok@users.noreply.github.com>
2026-07-01 23:27:59 -0700

    update icon

--- extensions/allporncomic/src/home.js
+++ extensions/allporncomic/src/home.js
@@ -2,7 +2,7 @@ load("config.js");
 
 function execute() {
     return Response.success([
-        { title: "Latest", input: BASE_URL + "/porncomic/?m_orderby=modified", script: "gen.js" },
+        { title: "Latest", input: BASE_URL + "/porncomic/", script: "gen.js" },
         { title: "Rating", input: BASE_URL + "/porncomic/?m_orderby=rating", script: "gen.js" },
         { title: "Trending", input: BASE_URL + "/porncomic/?m_orderby=trending", script: "gen.js" },
         { title: "Most viewed", input: BASE_URL + "/porncomic/?m_orderby=views", script: "gen.js" },
